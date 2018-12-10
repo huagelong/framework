f0203301b124efa4f4253dc1432144a56d5174a5
汪开辉 <hpuwang@gmail.com>
2018-12-10 10:36:28 +0800

    saas 功能添加

--- src/trensy/Foundation/Bootstrap/Session.php
+++ src/trensy/Foundation/Bootstrap/Session.php
@@ -14,9 +14,11 @@
 
 namespace Trensy\Foundation\Bootstrap;
 
+use Trensy\Di;
 use Trensy\Foundation\Storage\Redis;
 use Trensy\Config;
 use Trensy\Http\Session as HttpSession;
+use Trensy\Foundation\Exception\InvalidArgumentException;
 
 class Session extends HttpSession
 {
@@ -30,7 +32,16 @@ class Session extends HttpSession
         $serverConfig = Config::get("storage.server.redis.servers");
 
         if($serverConfig){
+            $initConfig = Config::get("app.session_init");
+            if ($initConfig) {
+                $obj = Di::get($initConfig);
+                if (!method_exists($obj, "perform")) {
+                    throw new InvalidArgumentException(" log class perform not config ");
+                }
+                call_user_func_array([$obj, "perform"], []);
+            }
             $config = Config::get("app.session");
+//            debug($config);
             $server = new Redis();
             parent::__construct($config, $server);
         }
